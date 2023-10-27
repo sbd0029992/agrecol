715c45b2c3b1ce697e9468d0ca617f368206383b
Diego Emerson Salvatierra Berrios <sbd0029992@est.univalle.edu>
2023-10-26 21:25:59 -0400

    [FEATURE] add product to admin navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -76,6 +76,14 @@ function Navbar() {
                       Dashboard
                     </Link>
                   </li>
+                  <li>
+                    <Link
+                      href='/product/list'
+                      className='block rounded py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500'
+                    >
+                      Productos
+                    </Link>
+                  </li>
                 </React.Fragment>
               )}
 
