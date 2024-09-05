56d470e14282b5ebcaf73b40ea52753b3ffa3a8e
Diego Salvatierra <ceixpp@gmail.com>
2024-09-05 16:54:54 -0400

    [FIX] delete cokies in logout

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -5,7 +5,7 @@ export default function LoadingSpinner() {
     <div role='status' className='flex flex-row items-center justify-center'>
       <svg
         aria-hidden='true'
-        className='mr-2 h-16 w-16 animate-spin fill-green-600 text-gray-200 dark:text-gray-600'
+        className='mr-2 h-10 w-16 animate-spin fill-green-600 text-gray-200 dark:text-gray-600'
         viewBox='0 0 100 101'
         fill='none'
         xmlns='http://www.w3.org/2000/svg'
--- src/pages/api/auth/logout.tsx
+++ src/pages/api/auth/logout.tsx
@@ -3,5 +3,10 @@ import withSession from 'lib/session';
 
 export default withSession(async (req: any, res: any) => {
   req.session.destroy();
+
+  res.setHeader('Set-Cookie', [
+    `root_auth_session=; expires=Thu, 01 Jan 1970 00:00:00 GMT; Path=/; HttpOnly`,
+  ]);
+
   res.redirect('/login');
 });
